dc5e28a24e7c2bcf798a7c9697ec6217dcdcc2b0
CityUhessnern <92000484+CityUhessnern@users.noreply.github.com>
2023-07-09 00:34:04 +0000

    Submission for Module 01 - Nikki Hessner

---
+++ PE01-Resume/resume/src/Resume.js
@@ -0,0 +1,58 @@
+import './Resume.css';
+
+function Resume () {
+    return (
+        <html>
+        <head>
+            <title>Student Name</title>
+        </head>
+        <body>
+            <h1>Nicole Hessner</h1> 
+            <contact>Spokane, WA 99203 | (509)380-9913 | hessnernicole@cityuniversity.edu</contact>
+            <h2>Education</h2>
+            <h3>Master of Science in Computer Science</h3>
+            <p>City University of Seattle, Seattle USA | September 2023</p>
+            <p>GPA: 3.9/4.0</p>
+            <h3>Graduate Certificate in Environmental Engineering</h3>
+            <p>Columbia University, New York USA | December 2017</p>
+            <p>GPA: 3.0/4.0</p>
+            <h3>Bachelor of Science in Mechanical Engineering</h3>
+            <p>Washington State University, Pullman, WA, USA | May 2015</p>
+            <p>GPA: 3.7/4.0</p>
+            <h2>Skills</h2>
+            <p>Programming Languages: Python, JavaScript, C#, C, ARM Assembly</p>
+            <p>Frameworks and Libraries: TensorFlow, PyTorch, scikit-learn, numpy, Pillow, Keras, React, React-Native, Node.js, Express.js</p>
+            <p>Database Systems: T-SQL, MongoDB, DynamoDB</p>
+            <p>Operating Systems: macOS, Linux, Windows</p>
+            <p>Version Control: Git</p>
+            <p>Configuration Management: Ansible</p>
+            <p>Project Management: Waterfall, Scrum, Kanban</p>
+            <p>Program Management</p>
+            <p>Computer Aided Design: SolidWorks, AutoCAD</p>
+            <p>Business Plan Development</p>
+            <p>Budget Planning</p>
+            <p>Energy Management</p>
+            <h2>Work Experience</h2>
+            <h3>Small Business Resiliency Consultant</h3>
+            <p>San Francisco Bureau of Commerce - Small Business Resiliency, San Francisco USA | October 2020 - April 2021</p>
+            <p>Advised a retail business franchisee on strategies and tactics to improve business outcomes during COVID-19 Pandemic. Franchise moved from second worst performing in the country to best.</p>
+            <h3>Facilities Mechanical Engineer</h3>
+            <p>Boeing, Everett, WA, USA | June 2015 - October 2020</p>
+            <p>Program Manager and Energy Manager for site energy and water conservation project portfolio. Lead cross-organizational and cross-departmental collaboration efforts resulting in 350,000 kWh/year energy reduction.</p>
+            <p>Project Manager for $20M site compressed air system upgrade.</p>
+            <p>Lead engineer for site hazardous energy placarding.</p>
+            <p>Co-author of Large-scale Data Integration for Facilities Analytics: Challenges and Opportunities.</p>
+            <h3>Facilities Mechanical Engineering Intern</h3>
+            <p>Boeing, Everett, WA, USA | May 2014 - August 2014</p>
+            <p>Updated HVAC monitoring system graphical UI within HoneyWell Enterprise Building Integrator.</p>
+            <h2>Projects</h2>
+            <h3>Large-scale Data Integration for Facilities Analytics: Challenges and Opportunities</h3>
+            <p>Requirements gathering for energy management application of big data.</p>
+            <p>Editor of paper.</p>
+            <p>Paper: <a href='https://ieeexplore.ieee.org/document/9378440'>https://ieeexplore.ieee.org/document/9378440</a></p>
+        </body>
+        </html>
+    );
+}
+
+export default Resume;
